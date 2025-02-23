97a9df912f12c23916d3ed339a936274a2c35984
Tun <116370663+tun784@users.noreply.github.com>
2025-02-23 23:21:55 +0700

    23-02-2025 23:21

--- helloworld/lib/main.dart
+++ helloworld/lib/main.dart
@@ -2,6 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:helloworld/user.dart';
 
 void main() {
-  User u = User(name: 'Tu', age: 20);
+  User u = User(name: 'T', age: 20);
   print('User: ${u.name} va ${u.age} tuoi');
 }
