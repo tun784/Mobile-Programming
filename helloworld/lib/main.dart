54d681b0cacbdfc144cd1a7a9271c07234b56972
Tun <116370663+tun784@users.noreply.github.com>
2025-06-21 22:22:28 +0700

    Delete helloworld/lib directory

--- helloworld/lib/main.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:helloworld/user.dart';
-
-void main() {
-  User u = User(name: 'T', age: 20);
-  print('User: ${u.name} va ${u.age} tuoi');
-}
